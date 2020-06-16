75f16c8b15bfff83e441be6b6f28b6421aed2973
feibang <1058677384@qq.com>
2020-06-16 19:39:47 +0800

    使用新的项目

---
+++ cinema/src/main/java/com/example/cinema/controller/home/Show.java
@@ -0,0 +1,9 @@
+package com.example.cinema.controller.home;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Show {
+}
